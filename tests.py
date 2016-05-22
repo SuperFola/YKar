295a7fe4f2d339dab650235a4f89cdfde70a49ac
Loodoor <folaefolc@outlook.fr>
2016-05-22 17:22:49 +0200

    source code

---
+++ tests.py
@@ -0,0 +1,73 @@
+# coding=utf-8
+
+__author__ = 'Alexandre Plateau'
+"""
+Licence MIT
+"""
+
+from ykar import *
+
+
+lis_tests = [
+    ("(say (testing 1 (2.0) -3.14e159))", "['testing', 1, [2.0], -3.14e+159]"),
+    ("(new f (open-input-file fichier.txt))", None),
+    ("(read-file f)", "texte"),
+    # ajouter les autres codes à tester (close-file, open-output-file, write-in-file, load-file)
+    ("(+ 2 2)", 4),
+    ("(+ (* 2 100) (* 1 10))", 210),
+    ("(if (> 6 5) (+ 1 1) (+ 2 2))", 2),
+    ("(if (< 6 5) (+ 1 1) (+ 2 2))", 4),
+    ("(new x 3)", None), ("(show x)", 3), ("(+ x x)", 6),
+    ("(begin (set! x 1) (set! x (+ x 1)) (+ x 1))", 3),
+    ("(new add (lambda ((x 1)) (+ x 1)))", None), ("(add)", 2), ("(add 2)", 3),
+    ("(new increment (lambda ((x 1) (k 1)) (+ x k)))", None),
+    ("(increment 10)", 11),
+    ("(increment 20 22)", 42),
+    ("(new kind (lambda ((x)) (+ x x)))", None),
+    ("(kind 5)", 10),
+    ("(new twice (lambda ((x)) (* 2 x)))", None), ("(twice 5)", 10),
+    ("(new compose (lambda ((f) (g)) (lambda (x) (f (g x)))))", None),
+    ("(new repeat (lambda ((f)) (compose f f)))", None),
+    ("(new fact (lambda ((n)) (if (<= n 1) 1 (* n (fact (- n 1))))))", None),
+    ("(fact 3)", 6),
+    ("(fact 50)", 30414093201713378043612608166064768844377641568960512000000000000),
+    ("(new abs2 (lambda ((n)) (if (> n 0) n (- 0 n))))", None),
+    ("(list (abs2 -3) (abs2 0) (abs2 3))", [3, 0, 3]),
+    ("""(new combine (lambda ((f))
+    (lambda ((x) (y))
+      (if (null? x) (say ())
+          (f (list (car x) (car y))
+             ((combine f) (cdr x) (cdr y)))))))""", None),
+    ("(new zip2 (combine cons))", None),
+    ("(zip (list 1 2 3 4) (list 5 6 7 8))", [(1, 5), (2, 6), (3, 7), (4, 8)]),
+    ("""(new riff-shuffle (lambda ((deck)) (begin
+    (new take (lambda ((n) (seq)) (if (<= n 0) (say ()) (cons (car seq) (take (- n 1) (cdr seq))))))
+    (new drop (lambda ((n) (seq)) (if (<= n 0) seq (drop (- n 1) (cdr seq)))))
+    (new mid (lambda ((seq)) (/ (length seq) 2)))
+    ((combine append) (take (mid deck) deck) (drop (mid deck) deck)))))""", None),
+    ("(riff-shuffle (list 1 2 3 4 5 6 7 8))", [1, 5, 2, 6, 3, 7, 4, 8])
+]
+
+
+def test(tests, name=''):
+    """For each (exp, expected) test case, see if eval(parse(exp)) == expected."""
+    fails = 0
+    env = standard_env()
+    for (x, expected) in tests:
+        result = "nothing"
+        try:
+            result = eval_code(parse(x), env)
+            print(x, '=>', to_string(result))
+            ok = (result == expected)
+        except Exception as e:
+            print(x, '=raises=>', type(e).__name__, e)
+            ok = False
+        if not ok:
+            fails += 1
+            print('FAIL!!!  Expected', expected, 'Got', result)
+        print()
+    print('%s %s: %d out of %d tests fail.' % ('*'*45, name, fails, len(tests)))
+
+
+if __name__ == '__main__':
+    test(lis_tests, 'YKar')
---
+++ ykar.py
@@ -0,0 +1,407 @@
+__author__ = 'Folaefolc'
+"""
+Code par Alexandre Plateau
+Licence MIT
+"""
+
+import math
+import cmath
+import operator as op
+import time
+
+
+start_token = '('
+end_token = ')'
+comment = ";"
+block_comment_token = "/*"
+block_comment_close_token = "*/"
+
+language_name = 'YKar'
+
+ev = {}
+pushed = {}
+is_checking_evs = False
+
+
+def to_string(x):
+    """Convert a Python object back into a Lisp-readable string."""
+    if x is True:
+        return "#t"
+    elif x is False:
+        return "#f"
+    elif isinstance(x, str):
+        return x
+    elif isinstance(x, str):
+        return '"%s"' % x.replace('"', r'\"')
+    elif isinstance(x, list):
+        return '(' + ' '.join(map(to_string, x)) + ')'
+    elif isinstance(x, complex):
+        return str(x).replace('j', 'i')
+    else:
+        return str(x)
+
+
+def recur_join(sep, x):
+    work = ""
+    for e in x:
+        if isinstance(e, list):
+            work += str(e)
+        else:
+            work += str(e) + sep
+    return work
+
+
+class Env(dict):
+    def __init__(self, parms=(), args=(), outer=None):
+        super().__init__(self)
+        self.outer = outer
+        try:
+            self.update(dict(parms))
+            parms_keys = tuple(dict(parms).keys())
+        except ValueError:
+            parms_keys = tuple([i[0] for i in parms])
+        self.update(zip(parms_keys, args))
+
+    def __getitem__(self, var):
+        return dict.__getitem__(self, var) if (var in self) else None
+
+    def find(self, var):
+        if var in self:
+            return self[var]
+        elif self.outer is not None:
+            return self.outer.find(var)
+        else:
+            raise_error('KeyError', '\'{}\' doesn\'t exist (with type : {})'.format(var, type(var).__name__))
+            return None
+
+
+class Procedure(object):
+    def __init__(self, params, body, envi):
+        for elem in params:
+            if not isinstance(elem, list):
+                raise_error("SyntaxError", "Missing brace arround argument '%s'" % elem)
+        self.params, self.body, self.env = params, body, envi
+
+    def __call__(self, *args):
+        return eval_code(self.body, Env(self.params, args, self.env))
+
+
+def raise_error(err_type, msg):
+    print(err_type, ':', msg)
+
+
+def tokenize(chars):
+    work = chars.replace(start_token, ' ( ').replace(end_token, ' ) ').split()
+    return work
+
+
+def parse(program):
+    tok = tokenize(program)
+    if '(' and ')' in tok:
+        return read_from_tokens(tok)
+    return raise_error('SyntaxError', 'Missing the brackets')
+
+
+def read_from_tokens(tokens):
+    if not len(tokens):
+        return raise_error('SyntaxError', 'Unexpected EOF while reading')
+
+    token = tokens.pop(0)
+
+    if token == block_comment_token:
+        while tokens[0] != block_comment_close_token:
+            tokens.pop(0)
+        tokens.pop(0)
+        token = tokens.pop(0)
+
+    if token == start_token:
+        ast = []
+        while tokens[0] != end_token:
+            ast.append(read_from_tokens(tokens))
+        tokens.pop(0)  # pop off ')'
+        return ast
+    elif token == end_token:
+        return raise_error('SyntaxError', 'Unexpected ' + token)
+    elif token == comment:
+        pass
+    else:
+        return atom(token)
+
+
+def atom(token):
+    if token == '#t':
+        return True
+    elif token == '#f':
+        return False
+    elif token[0] == '"':
+        return token[1:-1]
+    try:
+        return int(token)
+    except ValueError:
+        try:
+            return float(token)
+        except ValueError:
+            try:
+                return complex(token.replace('i', 'j', 1))
+            except ValueError:
+                return str(token)
+
+
+def standard_env():
+    global env
+    env = Env()
+    env.update(vars(math))  # sin, cos, sqrt, pi, ...
+    env.update(vars(cmath))
+    env.update({
+        '+': op.add, '-': op.sub, '*': op.mul, '/': op.truediv, '//': op.floordiv, '%': op.mod,
+        '>': op.gt, '<': op.lt, '>=': op.ge, '<=': op.le, '=': op.eq, '!=': op.ne,
+        'not': op.not_, 'eq?': op.is_, 'equal?': op.eq,
+        'abs': abs, 'zip': lambda *x: list(zip(*x)),
+        'car': lambda x: x[0], 'cdr': lambda x: x[1:], 'cons': lambda x, y: [x] + y,
+        'length': len, 'type': lambda x: type(x).__name__,
+        'list': lambda *x: list(x), 'list?': lambda x: isinstance(x, list),
+        'range': lambda start, stop: [_ for _ in range(start, stop + 1)],
+        'map': lambda *x: list(map(*x)), 'max': max, 'min': min,
+        'time': time.time, 'round': round,
+        'open-input-file': open, 'open-output-file': lambda f: open(f, 'w'), 'close-file': lambda f: f.close(),
+        'read-file': lambda f: f.read(), 'write-in-file': lambda f, s: f.write(s),
+        'load-file': lambda f: load(f),
+        'null': None, 'null?': lambda x: not not x,
+        'int': lambda x: int(x), 'float': lambda x: float(x),
+        'number?': lambda x: isinstance(x, (int, float)),
+        'bool': lambda x: bool(x), 'bool?': lambda x: isinstance(x, bool),
+        'procedure?': callable,
+        'symbol?': lambda x: isinstance(x, str)
+    })
+    return env
+
+
+def check_events():
+    global ev, pushed, is_checking_evs, env
+
+    if not is_checking_evs:
+        is_checking_evs = True
+        for name, (cond, body) in ev.items():
+            if pushed[name]['triggered'] or eval_code(cond, env):
+                tmp = eval_code(body, env)
+                if isinstance(tmp, Procedure):
+                    tmp(*[eval_code(a, env) for a in pushed[name]['args']])
+                pushed[name]['triggered'] = False
+    is_checking_evs = False
+
+
+def eval_code(x, env):
+    global ev
+
+    if isinstance(x, list):
+        if not x:
+            return raise_error('RuntimeError', 'Empty statement')
+
+    while True:
+        check_events()
+
+        if isinstance(x, str):
+            return env.find(x)
+        elif not isinstance(x, list):
+            return x
+        elif x[0] == "say":
+            if len(x) >= 2:
+                (_, *exp) = x
+                return recur_join(' ', exp)
+            else:
+                return raise_error("ArgumentError",
+                                   "'" + x[0] + "' need at least 1 argument")
+        elif x[0] == "show":
+            if len(x) == 2:
+                (_, exp) = x
+                return env.find(exp)
+            else:
+                return raise_error("ArgumentError",
+                                   "'" + x[0] + "' need exactly 1 argument")
+        elif x[0] == "lambda":
+            if len(x) == 3:
+                (_, params, body) = x
+                return Procedure(params, body, env)
+            else:
+                return raise_error("ArgumentError",
+                                   "'" + x[0] + "' need exactly 2 arguments")
+        elif x[0] == "if":
+            if len(x) == 4:
+                (_, test, conseq, alt) = x
+                x = conseq if eval_code(test, env) else alt
+            if len(x) == 3:
+                (_, test, conseq) = x
+                if eval_code(test, env):
+                    x = conseq
+            else:
+                return raise_error("ArgumentError",
+                                   "'" + x[0] + "' need between 2 and 3 arguments")
+        elif x[0] == "?":
+            if len(x) == 2:
+                (_, test) = x
+                x = 1 if eval_code(test, env) else 0
+            else:
+                return raise_error("ArgumentError",
+                                   "'" + x[0] + "' need exactly 1 argument")
+        elif x[0] == "new":
+            if len(x) == 3 or len(x) == 4 and x[2] in env.keys():
+                (_, var, *exp) = x
+                if var not in env.keys():
+                    tmp = eval_code(exp[0], env)
+                    if tmp:
+                        env[var] = tmp
+                        return None
+                    else:
+                        return raise_error('RuntimeError', 'The wanted value is impossible to create')
+                else:
+                    return raise_error("DefineError",
+                                       "Can't override existing variable. Use set! instead")
+            else:
+                return raise_error("ArgumentError",
+                                   "'" + x[0] + "' need exactly 2 arguments")
+        elif x[0] == "new-event":
+            if len(x) == 3:
+                (_, var, cond, exp) = x
+                if var not in ev.keys():
+                    ev[var] = [eval_code(cond, env), eval_code(exp, env)]
+                    pushed[var]['args'] = []
+                    pushed[var]['triggered'] = False
+                    return None
+                else:
+                    return raise_error("DefineError",
+                                       "Can't override existing event-driven variable. Use set!-event instead")
+            else:
+                return raise_error("ArgumentError",
+                                   "'" + x[0] + "' need exactly 2 arguments")
+        elif x[0] == "set!":
+            if len(x) == 3 or len(x) == 4 and x[2] in env.keys():
+                (_, var, *exp) = x
+                if var in env.keys():
+                    tmp = eval_code(exp[0], env)
+                    if tmp:
+                        env[var] = tmp
+                        return None
+                    else:
+                        return raise_error('RuntimeError', 'The wanted value is impossible to create')
+                else:
+                    return raise_error("SetError", "Can't overwrite a non existing variable. Use new instead")
+            else:
+                return raise_error("ArgumentError",
+                                   "'" + x[0] + "' need exactly 2 arguments")
+        elif x[0] == "set!-event":
+            if len(x) == 3:
+                (_, var, cond, exp) = x
+                if var in ev.keys():
+                    ev[var] = [eval_code(cond, env), eval_code(exp, env)]
+                    pushed[var]['args'] = []
+                    pushed[var]['triggered'] = False
+                    return None
+                else:
+                    return raise_error("SetError",
+                                       "Can't overwrite a non existing event-driven variable. Use new-event instead")
+            else:
+                return raise_error("ArgumentError",
+                                   "'" + x[0] + "' need exactly 2 arguments")
+        elif x[0] == "push":
+            if len(x) >= 2:
+                (_, var, args) = x
+                if isinstance(ev[var], Procedure):
+                    args = [eval_code(exp, env) for exp in args]
+                    pushed[var]['args'] = args
+                    pushed[var]['triggered'] = True
+            else:
+                return raise_error("ArgumentError",
+                                   "'" + x[0] + "' need at least 2 arguments")
+        elif x[0] == "del":
+            if len(x) >= 2:
+                (_, *exp) = x
+                for e in exp:
+                    if e in env.keys():
+                        del env[e]
+                    elif e in ev.keys():
+                        del env[e]
+                    else:
+                        raise_error('ArgumentError', '\'%s\' does not exist' % e)
+                return None
+            else:
+                return raise_error("ArgumentError",
+                                   "'" + x[0] + "' need at least 1 argument")
+        elif x[0] == "begin":
+            for exp in x[1:]:
+                eval_code(exp, env)
+            x = x[-1]
+        elif x[0] == "symbol":
+            return recur_join(' ', x[1:])
+        elif x[0] == "env":
+            return sorted(list(env.keys()))
+        else:
+            if not isinstance(env.find(x[0]), str):
+                exps = [eval_code(exp, env) for exp in x]
+                proc = exps.pop(0)
+                if isinstance(proc, Procedure):
+                    x = proc.body
+                    env = Env(proc.params, exps, proc.env)
+                else:
+                    try:
+                        return proc(*exps)
+                    except Exception as e:
+                        if isinstance(e, TypeError):
+                            return raise_error('RuntimeError', e)
+                        return raise_error(type(e).__name__, e)
+
+
+def loop(env):
+    std_prompt = language_name + ' > '
+    not_eof_prompt = language_name + ' \' '
+
+    prompt = std_prompt
+    code = ""
+
+    while True:
+        code = input(prompt) if prompt != not_eof_prompt else code + " " + input(prompt)
+
+        if code.count(start_token) != code.count(end_token):
+            prompt = not_eof_prompt
+
+            if code in env.keys():
+                prompt = std_prompt
+
+        if code.count(start_token) == code.count(end_token) and code.strip()[:2] != comment:
+            prompt = std_prompt
+
+            parsed = parse(code)
+            val = eval_code(parsed, env)
+
+            if val is not None:
+                print(schemestr(val))
+
+
+def load(file):
+    try:
+        with open(file) as f:
+            for line in f.readline():
+                if line.count(start_token) == line.count(end_token) and line.strip()[:2] != comment:
+                    parsed = parse(line)
+                    val = eval_code(parsed, env)
+
+                    if val is not None:
+                        print(schemestr(val))
+    except OSError:
+        raise_error('OSError', '\'%s\' does not exists' % file)
+
+
+def schemestr(exp):
+    return str(exp) if not isinstance(exp, list) else str(exp)[1:-1]
+
+
+if __name__ == '__main__':
+    t = [
+        "db    db  db   dD   .d8b.   d8888b.", "`8b  d8'  88 ,8P'  d8' `8b  88  `8D",
+        " `8bd8'   88,8P    88ooo88  88oobY'", "   88     88`8b    88~~~88  88`8b  ",
+        "   88     88 `88.  88   88  88 `88.", "   YP     YP   YD  YP   YP  88   YD"
+    ]
+    i = ["Version 0.1", "Développé par Folaefolc", "", "", "", "(env) pour lister toutes les fonctions"]
+    print("\n".join(" " * 4 + t[c] + " " * 8 + "- " + i[c] for c in range(len(t))), "\n")
+    env = standard_env()
+    global env
+    loop(env)
