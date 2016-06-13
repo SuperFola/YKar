c5e5e7d2a5a157806f3538927ca215cae14b01f7
Loodoor <folaefolc@outlook.fr>
2016-06-13 22:21:33 +0200

    require, Nil, environement

--- tests.py
+++ tests.py
@@ -9,60 +9,68 @@ from ykar import *
 
 
 lis_tests = [
-    ("(say (testing 1 (2.0) -3.14e159))", "['testing', 1, [2.0], -3.14e+159]"),
-    ("(new f1 (open-output-file (symbol fichier.txt)))", None),
-    ("(write-in-file f1 (symbol hello world !))", 13),
-    ("(close-file f1)", None),
-    ("(new f (open-input-file (symbol fichier.txt)))", None),
-    ("(new f3 (read-file f))", None),
-    ("(show f3)", "hello world !"),
-    ("(match f3 ((symbol hello world) (- 0 1)) ((symbol hello world  !) (+ 2 2)) ((symbol hello world !) (+ 3 2)))", 5),
-    # ajouter les autres codes à tester (load-file)
-    ("(call/cc (lambda ((throw)) (+ 5 (* 10 (throw 1))))) ;; throw", 1),
-    ("(call/cc (lambda ((throw)) (+ 5 (* 10 1)))) ;; do not throw", 15),
-    ("""(call/cc (lambda ((throw))
-             (+ 5 (* 10 (call/cc (lambda ((escape)) (* 100 (escape 3)))))))) ; 1 level""", 35),
-    ("""(call/cc (lambda ((throw))
-             (+ 5 (* 10 (call/cc (lambda ((escape)) (* 100 (throw 3)))))))) ; 2 levels""", 3),
-    ("""(call/cc (lambda ((throw))
-             (+ 5 (* 10 (call/cc (lambda ((escape)) (* 100 1))))))) ; 0 levels""", 1005),
+    ("(quote (testing 1 (2.0) -3.14e159))", "(\"testing\" 1 (2.0) -3.14e+159)"),
     ("(+ 2 2)", 4),
     ("(+ (* 2 100) (* 1 10))", 210),
     ("(if (> 6 5) (+ 1 1) (+ 2 2))", 2),
     ("(if (< 6 5) (+ 1 1) (+ 2 2))", 4),
     ("(new x 3)", None), ("(show x)", 3), ("(+ x x)", 6),
     ("(begin (set! x 1) (set! x (+ x 1)) (+ x 1))", 3),
-    ("(new add (lambda ((x 1)) (+ x 1)))", None), ("(add)", 2), ("(add 2)", 3),
-    ("(new increment (lambda ((x 1) (k 1)) (+ x k)))", None),
+    ("(new add (lambda (x 1) (+ x 1)))", None), ("(add)", 2), ("(add 2)", 3),
+    ("(new increment (lambda (x 1) (k 1) (+ x k)))", None),
     ("(increment 10)", 11),
     ("(increment 20 22)", 42),
-    ("(new kind (lambda ((x)) (+ x x)))", None),
+    ("(new kind (lambda x (+ x x)))", None),
     ("(kind 5)", 10),
-    ("(new twice (lambda ((x)) (* 2 x)))", None), ("(twice 5)", 10),
-    ("(new compose (lambda ((f) (g)) (lambda ((x)) (f (g x)))))", None),
-    ("(new repeat (lambda ((f)) (compose f f)))", None),
+    ("(new twice (lambda x (* 2 x)))", None), ("(twice 5)", 10),
+    ("(new compose (lambda f g (lambda x (f (g x)))))", None),
+    ("(new repeat (lambda f (compose f f)))", None),
     ("(new test (repeat twice))", None),
     ("(test 2)", 8),
-    ("(new fact (lambda ((n)) (if (<= n 1) 1 (* n (fact (- n 1))))))", None),
+    ("(new fact (lambda n (if (<= n 1) 1 (* n (fact (- n 1))))))", None),
     ("(fact 3)", 6),
     ("(fact 50)", 30414093201713378043612608166064768844377641568960512000000000000),
-    ("(new abs2 (lambda ((n)) (if (> n 0) n (- 0 n))))", None),
+    ("(new abs2 (lambda n (if (> n 0) n (- 0 n))))", None),
     ("(list (abs2 -3) (abs2 0) (abs2 3))", [3, 0, 3]),
-    ("""(new combine (lambda ((f))
-    (lambda ((x) (y))
-      (if (null? x) (say ())
-          (f (list (car x) (car y))
-             ((combine f) (cdr x) (cdr y)))))))""", None),
-    ("(new zip2 (combine cons))", None),
-    ("(zip (list 1 2 3 4) (list 5 6 7 8))", [(1, 5), (2, 6), (3, 7), (4, 8)]),
-    ("""(new riff-shuffle (lambda ((deck)) (begin
-        (new take (lambda ((n) (seq)) (if (<= n 0) (say 0) (cons (car seq) (take (- n 1) (cdr seq))))))
-        (new drop (lambda ((n) (seq)) (if (<= n 0) seq (drop (- n 1) (cdr seq)))))
-        (new mid (lambda ((seq)) (/ (length seq) 2)))
-        (new combi-append (combine append))
-        (combi-append (take (mid deck) deck) (drop (mid deck) deck)))))""", None),
-    ("(riff-shuffle (list 1 2 3 4 5 6 7 8))", [1, 5, 2, 6, 3, 7, 4, 8])
+    ("""(new combine (lambda f
+            (lambda x y
+              (if (null? x) Nil
+                  (f (list (car x) (car y))
+                     ((combine f) (cdr x) (cdr y)))))))""", None),
+    ("((combine cons) (list 1 2 3 4) (list 5 6 7 8))", [(1, 5), (2, 6), (3, 7), (4, 8)]),
+    ("(new take (lambda n seq (if (<= n 0) 0 (cons (car seq) (take (- n 1) (cdr seq))))))", None),
+    ("(new drop (lambda n seq (if (<= n 0) seq (drop (- n 1) (cdr seq)))))", None),
+    ("(new mid (lambda seq (/ (length seq) 2)))", None),
+    ("(new riff-shuffle (lambda deck ((combine append) (drop (mid deck) deck) (take (mid deck) deck))))", None),
+    ("(riff-shuffle (list 1 2 3 4 5 6 7 8))", [1, 5, 2, 6, 3, 7, 4, 8]),
+    ("(new f1 (open-output-file (quote fichier.txt)))", None),
+    ("(write-in-file f1 (quote hello world !))", 13),
+    ("(close-file f1)", None),
+    ("(new f (open-input-file (quote fichier.txt)))", None),
+    ("(new f3 (read-file f))", None),
+    ("(show f3)", "hello world !"),
+    ("(match f3 ((quote hello world) (- 0 1)) ((quote hello world  !) (+ 2 2)) ((quote hello world !) (+ 3 2)))", 5),
+    ("(call/cc (lambda throw (+ 5 (* 10 (throw 1))))) ;; throw", 1),
+    ("(call/cc (lambda throw (+ 5 (* 10 1)))) ;; do not throw", 15),
+    ("""(call/cc (lambda throw
+                 (+ 5 (* 10 (call/cc (lambda escape (* 100 (escape 3)))))))) ; 1 level""", 35),
+    ("""(call/cc (lambda throw
+                 (+ 5 (* 10 (call/cc (lambda escape (* 100 (throw 3)))))))) ; 2 levels""", 3),
+    ("""(call/cc (lambda throw
+                 (+ 5 (* 10 (call/cc (lambda escape (* 100 1))))))) ; 0 levels""", 1005),
+    ("(new first car)", None),
+    ("(new rest cdr)", None),
+    ("(new count (lambda item L (if L (+ (equal? item (first L)) (count item (rest L))) 0)))", None),
+    ("(count 0 (list 0 1 2 3 0 0))", 3),
+    ("(count (quote the) (quote (the more the merrier the bigger the better)))", 4),
+    ("((lambda n (+ n 1)) 4)", 5),
+    ("""(new my-range (lambda start end
+        (if (= start end)
+            Nil
+            (cons start (range (+ start 1) end)))))""", None),
+    ("(my-range 1 5)", "(1 2 3 4)")
 ]
+# ajouter les autres codes à tester (load-file)
 
 
 def test(tests, name=''):
@@ -73,16 +81,12 @@ def test(tests, name=''):
         result = "nothing"
         try:
             result = eval_code(parse(x), env)
-            print(x, '=>', to_string(result))
-            ok = (result == expected)
+            # print(x, '=>', to_string(result))
         except Exception as e:
-            print(x, '=raises=>', type(e).__name__, e)
-            ok = False
-        if not ok:
+            print(x, '=raises=>', type(e).__name__, "->>", e)
             fails += 1
-            print('FAIL!!!  Expected', expected, 'Got', result)
-        print()
-    print('%s %s: %d out of %d tests fail.' % ('*'*45, name, fails, len(tests)))
+            print('FAIL!!!  Expected', expected, 'Got', result, "\n")
+    print('%s %s: %d out of %d tests fail.' % ('*' * 45, name, fails, len(tests)))
 
 
 if __name__ == '__main__':
--- ykar.py
+++ ykar.py
@@ -75,8 +75,7 @@ class Env(dict):
 class Procedure(object):
     def __init__(self, params, body, envi):
         for elem in params:
-            if not isinstance(elem, list):
-                raise_error("SyntaxError", "Missing brace arround argument '%s'" % elem)
+            require(isinstance(elem, list), SyntaxError, "Missing brace arround argument '%s'" % elem)
         self.params, self.body, self.env = params, body, envi
 
     def __call__(self, *args):
@@ -89,51 +88,23 @@ def to_string(x):
         return "#t"
     elif x is False:
         return "#f"
+    elif x is None:
+        return "Nil"
     elif isinstance(x, str):
         return x
-    elif isinstance(x, str):
-        return '"%s"' % x.replace('"', r'\"')
     elif isinstance(x, list):
-        return '(' + ' '.join(map(to_string, x)) + ')'
+        return '(' + ' '.join(to_string(i) for i in x) + ')'
     elif isinstance(x, complex):
         return str(x).replace('j', 'i')
     else:
         return str(x)
 
 
-def recur_join(sep, x):
-    work = ""
-    for e in x:
-        if isinstance(e, list):
-            work += str(e)
-        else:
-            work += str(e) + sep
-    return work[:-1] if work[0] != '[' else work
-
-
 def tokenize(chars):
     for tok in tokens:
         chars = chars.replace(tok, ' %s ' % tok)
 
-    work = []
-    word = ""
-    sym_tok = False
-    for i, c in enumerate(chars):
-        if sym_tok:
-            if i + 1 < len(chars):
-                if chars[i + 1] == end_token:
-                    sym_tok = False
-        if (c != " " or sym_tok) and c not in ('\r', '\n', '\r\n'):
-            word += c
-        else:
-            if not sym_tok and word:
-                work.append(word)
-            if word == "symbol":
-                sym_tok = True
-                word = ""
-            if not sym_tok:
-                word = ""
-    return work
+    return chars.split()
 
 
 def parse(code):
@@ -173,6 +144,8 @@ def atom(token):
         return True
     elif token == '#f':
         return False
+    elif token == "Nil":
+        return None
     try:
         return int(token)
     except ValueError:
@@ -186,36 +159,49 @@ def atom(token):
 
 
 def standard_env():
-    env = Env()
-    env.update(vars(math))  # sin, cos, sqrt, pi, ...
-    env.update(vars(cmath))
-    env.update({
-        '+': op.add, '-': op.sub, '*': op.mul, '/': op.truediv, '//': op.floordiv, '%': op.mod,
+    _env = Env()
+    _env.update(vars(math))  # sin, cos, sqrt, pi, ...
+    _env.update(vars(cmath))
+    _env.update({
+        "+": lambda a, b: op.add(a, b), "-": lambda a, b: op.sub(a, b),
+        "/": lambda a, b: op.truediv(a, b), "*": lambda a, b: op.mul(a, b),
+        "//": lambda a, b: op.floordiv(a, b), "%": lambda a, b: op.mod(a, b),
+        "pow": lambda x, p: x ** p,
+
+        "^": lambda a, b: op.xor(a, b), "|": lambda a, b: op.or_(a, b),
+        "&": lambda a, b: op.and_(a, b), "~": lambda x: ~x,
+        ">>": lambda x, dc: x >> dc, "<<": lambda x, dc: x << dc,
+
         '>': op.gt, '<': op.lt, '>=': op.ge, '<=': op.le, '=': op.eq, '!=': op.ne,
-        'append': op.add,
         'not': op.not_, 'eq?': op.is_, 'equal?': op.eq,
-        'abs': abs, 'zip': lambda *x: list(zip(*x)),
-        'car': lambda x: x[0], 'cdr': lambda x: x[1:], 'cons': lambda x, y: [x] + y,
-        'length': len, 'type': lambda x: type(x).__name__,
+
+        'ord': ord, 'chr': chr,
+
+        '#': op.getitem, '#=': op.setitem, '#~': op.delitem, 'length': len,
         'list': lambda *x: list(x), 'list?': lambda x: isinstance(x, list),
-        'range': lambda start, stop: [_ for _ in range(start, stop + 1)],
+        'append': op.add, 'car': lambda x: x[0], 'cdr': lambda x: x[1:], 'cons': lambda x, y: [x] + y,
+        'join': lambda char, li: char.join(str(e) for e in li),
+
+        'time': time.time, 'round': round, 'abs': abs, 'zip': lambda *x: list(zip(*x)),
+        'type': lambda x: type(x).__name__, 'range': lambda start, stop: [_ for _ in range(start, stop + 1)],
         'map': lambda *x: list(map(*x)), 'max': max, 'min': min,
-        'time': time.time, 'round': round,
+
         'open-input-file': open, 'open-output-file': lambda f: open(f, 'w'), 'close-file': lambda f: f.close(),
         'read-file': lambda f: f.read(), 'write-in-file': lambda f, s: f.write(s),
         'load-file': lambda f: load(f),
-        'null': None, 'null?': lambda x: not not x,
-        'int': lambda x: int(x), 'float': lambda x: float(x),
-        'number?': lambda x: isinstance(x, (int, float)),
+
+        'null': None, 'null?': lambda x: bool(x),
+        'int': lambda x: int(x), 'float': lambda x: float(x), 'number?': lambda x: isinstance(x, (int, float)),
         'bool': lambda x: bool(x), 'bool?': lambda x: isinstance(x, bool),
         'procedure?': callable,
         'symbol?': lambda x: isinstance(x, str),
+
         'call/cc': callcc
     })
-    return env
+    return _env
 
 
-def parse_use_instruction(env, code):
+def parse_use_instruction(_env, code):
     full_ns = []
     list_content = []
 
@@ -240,47 +226,43 @@ def parse_use_instruction(env, code):
     require(os.path.exists(os.path.join(*full_ns)),
             OSError, 'Lib can not be found at \'%s\'' % os.path.join(os.getcwd(), *full_ns))
     if not list_content:
-        env[namespace_tok.join(full_ns)] = load_env_from_file(os.path.join(*full_ns)) ^ standard_env()
+        _env[namespace_tok.join(full_ns)] = load_env_from_file(os.path.join(*full_ns)) ^ standard_env()
     else:
         modules = Env()
         modules.update(zip(list_content, itertools.repeat(None)))
-        env[namespace_tok.join(full_ns)] = load_env_from_file(os.path.join(*full_ns)) & modules
+        _env[namespace_tok.join(full_ns)] = load_env_from_file(os.path.join(*full_ns)) & modules
 
 
 def callcc(proc):
     ball = RuntimeWarning("Sorry, can't continue this continuation any longer.")
     r = None
+    _err = False
 
     def throw(retval):
-        nonlocal r
+        nonlocal r, _err
         r = retval
+        _err = True
         raise ball
 
     try:
-        print(throw)
         return proc(throw)
     except RuntimeWarning as w:
-        print(r)
-        if w is ball:
-            print('in')
+        if _err or w is ball:
             return r
         else:
             raise_error(type(w).__name__, w)
 
 
 def eval_code(x, env):
-    if isinstance(x, list):
-        require(bool(x), RuntimeError, 'Empty statement')
-
     while True:
         if isinstance(x, str):
             return env.find(x)
         elif not isinstance(x, list):
             return x
-        elif x[0] == "say":
-            require(len(x) >= 2, ValueError, "'say' need at least 1 argument, got %i argument(s)" % (len(x) - 1))
+
+        elif x[0] == "quote":
             (_, *exp) = x
-            return recur_join(' ', exp)
+            return " ".join(to_string(e) for e in exp)
         elif x[0] == "use":
             require(len(x) >= 2, ValueError, "'use' need at least 1 argument, got %i argument(s)" % (len(x) - 1))
             (_, *exp) = x
@@ -299,8 +281,14 @@ def eval_code(x, env):
                     return eval_code(new_code, env)
             return None
         elif x[0] == "lambda":
-            require(len(x) == 3, ValueError, "'lambda' need exactly 2 arguments, got %i argument(s)" % (len(x) - 1))
-            (_, params, body) = x
+            require(len(x) >= 3, ValueError, "'lambda' need at least 2 arguments, got %i argument(s)" % (len(x) - 1))
+            x.pop(0)
+            body = x.pop(-1)
+            params = x
+            for p in params:
+                if not isinstance(p, list):
+                    params = [[_p] for _p in params]
+                    break
             return Procedure(params, body, env)
         elif x[0] == "if":
             require(3 <= len(x) <= 4, ValueError, "'if' need between 2 and 3 arguments, got %i argument(s)" % (len(x) - 1))
@@ -316,7 +304,7 @@ def eval_code(x, env):
             (_, var, *exp) = x
             require(var not in env.keys(), RuntimeError, "Can not overwrite existing variable, use set! instead")
             tmp = eval_code(exp[0], env)
-            require(bool(tmp), RuntimeError, "Impossible to create the value")
+            require(tmp is not None, RuntimeError, "Impossible to create the value. Expression was : %s > %s" % (exp, tmp))
             env[var] = tmp
             return None
         elif x[0] == "set!":
@@ -324,7 +312,7 @@ def eval_code(x, env):
             (_, var, *exp) = x
             require(var in env.keys(), RuntimeError, "Can not overwrite non existing variable, use new instead")
             tmp = eval_code(exp[0], env)
-            require(bool(tmp), RuntimeError, "Impossible to create the value")
+            require(tmp is not None, RuntimeError, "Impossible to create the value. Expression was : %s > %s" % (exp, tmp))
             env[var] = tmp
             return None
         elif x[0] == "del":
@@ -338,19 +326,17 @@ def eval_code(x, env):
             for exp in x[1:]:
                 eval_code(exp, env)
             x = x[-1]
-        elif x[0] == "symbol":
-            return recur_join(' ', x[1:])
         elif x[0] == "env":
             return sorted(list(env.keys()))
+
         else:
-            if not isinstance(env.find(x[0]), str):
-                exps = [eval_code(exp, env) for exp in x]
-                proc = exps.pop(0)
-                if isinstance(proc, Procedure):
-                    x = proc.body
-                    env = Env(proc.params, exps, proc.env)
-                else:
-                    return proc(*exps)
+            exps = [eval_code(exp, env) for exp in x]
+            proc = exps.pop(0)
+            if isinstance(proc, Procedure):
+                x = proc.body
+                env = Env(proc.params, exps, proc.env)
+            else:
+                return proc(*exps)
 
 
 def load_env_from_file(path):
@@ -390,7 +376,8 @@ def loop(env):
             try:
                 val = eval_code(parsed, env)
             except Exception as exc:
-                print(type(exc).__name__, exc)
+                raise_error(type(exc).__name__, exc)
+                val = None
 
             if val is not None:
                 print(schemestr(val))
